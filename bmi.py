bcdd54d4f2e798c2c26186e32138c2b639e74d44
hamuthesh <rlhamuthesh.24@ichat.sp.edu.sg>
2025-05-08 12:13:26 +0800

    bmi classification

--- bmi.py
+++ bmi.py
@@ -4,4 +4,11 @@ def calculate_bmi(height, weight):
 
     bmi = weight/(height * height)
     print("BMI:" + str(bmi))
+
+    if bmi < 18.5:
+        print("Under Weight")
+    elif bmi >= 18.5 or bmi <= 25:
+        print("Normal Weight")
+    else:
+        print("Over Weight")
 calculate_bmi(weight=57, height=1.73)
