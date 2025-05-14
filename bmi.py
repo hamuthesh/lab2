24c67afbc0705e3624b8d21fd9a93db3e6dce46d
hamuthesh <rlhamuthesh.24@ichat.sp.edu.sg>
2025-05-14 19:05:42 +0800

    changes from lab2

--- bmi.py
+++ bmi.py
@@ -7,8 +7,11 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Under Weight")
+        return -1
     elif bmi >= 18.5 or bmi <= 25:
         print("Normal Weight")
+        return 1
     else:
         print("Over Weight")
+        return 0
 calculate_bmi(weight=57, height=1.73)
